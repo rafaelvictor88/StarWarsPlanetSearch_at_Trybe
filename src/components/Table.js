e946dd6545cbd28352aacfaa0d765f9166d5683d
Rafael Victor <rafavictorgs@gmail.com>
2021-10-05 16:55:59 -0300

    Resolvendo erros de Lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -22,26 +22,21 @@ function Table() {
   const tableBody = () => (
     data.length === 0
       ? 'carregando'
-      : data.map(({
-        name, rotation_period, orbital_period,
-        diameter, climate, gravity,
-        terrain, surface_water, population,
-        films, created, edited, url,
-      }, index) => (
+      : data.map((planet, index) => (
         <tr key={ index }>
-          <td>{ name }</td>
-          <td>{ rotation_period }</td>
-          <td>{ orbital_period }</td>
-          <td>{ diameter }</td>
-          <td>{ climate }</td>
-          <td>{ gravity }</td>
-          <td>{ terrain }</td>
-          <td>{ surface_water }</td>
-          <td>{ population }</td>
-          <td>{ films }</td>
-          <td>{ created }</td>
-          <td>{ edited }</td>
-          <td>{ url }</td>
+          <td>{ planet.name }</td>
+          <td>{ planet.rotation_period }</td>
+          <td>{ planet.orbital_period }</td>
+          <td>{ planet.diameter }</td>
+          <td>{ planet.climate }</td>
+          <td>{ planet.gravity }</td>
+          <td>{ planet.terrain }</td>
+          <td>{ planet.surface_water }</td>
+          <td>{ planet.population }</td>
+          <td>{ planet.films }</td>
+          <td>{ planet.created }</td>
+          <td>{ planet.edited }</td>
+          <td>{ planet.url }</td>
         </tr>
       ))
   );
