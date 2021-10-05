25959fb896ca48bd05656e5a70688d0ad5da6bdd
Rafael Victor <rafavictorgs@gmail.com>
2021-10-05 18:40:30 -0300

    Iniciando os requisitos 2 e 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import PlanetProvider from './context/PlanetProvider';
 import Table from './components/Table';
 import Header from './components/Header';
 import FilterByName from './components/FilterByName';
+import FilterByNumeric from './components/FilterByNumeric';
 
 function App() {
   return (
     <PlanetProvider>
       <Header />
       <FilterByName />
+      <FilterByNumeric />
       <Table />
     </PlanetProvider>
   );
--- src/components/FilterByNumeric.js
+++ src/components/FilterByNumeric.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 function FilterByNumeric() {
+  handleClickNumericFilter = () => {};
   return (
     <div>
-      <label htmlFor="name-filter">
-        Pesquise pelo nome do planeta:
-        <input
-          type="text"
-          name="name-filter"
-          data-testid="name-filter"
-          onChange={ handleNameFilter }
-        />
-      </label>
+      <select data-testid="column-filter">
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_period">surface_water</option>
+      </select>
+      <select data-testid="comparison-filter">
+        <option value="maior-que">maior que</option>
+        <option value="menor-que">menor que</option>
+        <option value="igual-a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
     </div>
   );
 }
